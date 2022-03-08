e44961d83141b3da89edd77926dcc3d2a3f60198
Gurivireddym <97427817+Gurivireddym@users.noreply.github.com>
2022-03-08 09:15:29 +0530

    Update UrlShortnerAppl.py

--- UrlShortnerAppl.py
+++ UrlShortnerAppl.py
@@ -3,30 +3,30 @@ con = sqlite3.connect("app.db")
 
 class URLShortnerApp:
 
-    id=100000000000
+    id = 100000000000
     urls_list = {}
     cursor = con.execute(''' SELECT url,shorturl,id from urldb;''')
-    c=0
+    c = 0
     for i in cursor:
-        urls_list[i[0]]=i[2]
+        urls_list[i[0]] = i[2]
         id = i[2]
-        c +=1
+        c += 1
     id +=1
-    def short_url(self,original_url):
+    def ShortURL(self,original_url):
         
         if original_url in self.urls_list:
-            id=self.urls_list[original_url]
-            shorten_url = self.id_range(id)
+            id = self.urls_list[original_url]
+            shorten_url = self.IdRange(id)
         
         else:
-            self.urls_list[original_url]=self.id
-            shorten_url = self.id_range(self.id)
+            self.urls_list[original_url] = self.id
+            shorten_url = self.IdRange(self.id)
             self.id += 1
         
         return "shorten_url/"+ shorten_url
         
 
-    def id_range(self,id):
+    def IdRange(self,id):
         char = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         base = len(char)
         data = []
@@ -36,16 +36,16 @@ class URLShortnerApp:
             id = id // base
         return "".join(data[::-1])
 
-shortner= URLShortnerApp()
+shortner = URLShortnerApp()
 
 a = input("Enter your url: ")
-print(shortner.short_url(a))
+print(shortner.ShortURL(a))
 
 var = con.execute(''' SELECT url,shorturl,id from urldb;''')
 count = 1
 for i in var:
     
-    if (shortner.urls_list[i[0]]==shortner.urls_list[a]):
+    if (shortner.urls_list[i[0]] == shortner.urls_list[a]):
         print("Entered URL is already available in database..")
         print("your shorten url is: ",i[1])
         break
@@ -55,7 +55,7 @@ for i in var:
         insert = ''' INSERT INTO urldb(id,url,shorturl)
               VALUES(?,?,?) '''
 
-        data = (shortner.urls_list[a],a,shortner.short_url(a))
+        data = (shortner.urls_list[a],a,shortner.ShortURL(a))
         cur = con.cursor()
         cur.execute(insert, data)
         con.commit()   
